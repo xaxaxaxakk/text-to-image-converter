ea17fbf5073040dd94e72e049b130812e0d3984f
xaxaxaxakk <100176511+xaxaxaxakk@users.noreply.github.com>
2025-05-03 02:29:21 +0900

    v1.2.8

--- index.js
+++ index.js
@@ -1379,4 +1379,14 @@ jQuery(async () => {
   $("#clear_replace").on("click", () => {
     $(".replacer_box").val("");
   });
+
+  $(".tab-btn").click(function () {
+    $(".tab-btn").removeClass("active");
+    $(".tab-content").removeClass("active");
+
+    $(this).addClass("active");
+    const tabId = $(this).data("tab");
+    $("#" + tabId).addClass("active");
+  });
+  $(".tab-btn").first().click();
 });
